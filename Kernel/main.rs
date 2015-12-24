c7e2a783a3c068f070d77cb215cde5e05041e47f
John Hodge <tpg@mutabah.net>
2015-12-24 19:14:07 +0800

    Remove a collection of warnings, rewrite port IO functions to allow immediate form.

--- Kernel/main.rs
+++ Kernel/main.rs
@@ -8,10 +8,8 @@
  * This code has been put into the public domain, there are no restrictions on
  * its use, and the author takes no liability.
  */
-#![feature(no_std)]	//< unwind needs to define lang items
 #![feature(lang_items)]	//< unwind needs to define lang items
 #![feature(asm)]	//< As a kernel, we need inline assembly
-#![feature(core_str_ext)]   
 #![no_std]	//< Kernels can't use std
 
 /// A dummy 'std' module to work around a set of issues in rustc
